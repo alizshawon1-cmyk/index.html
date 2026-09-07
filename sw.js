50cab85e3a710c961fa81d10360a07b333703339
alizshawon1-cmyk <alizshawon1@gmail.com>
2026-09-07 22:30:33 +0600

    Add service worker for SHAWON TV
    Implement a service worker for caching assets.

---
+++ sw.js
@@ -0,0 +1,27 @@
+// Service Worker for SHAWON TV
+const CACHE_NAME = 'shawon-tv-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch/Cache Requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
